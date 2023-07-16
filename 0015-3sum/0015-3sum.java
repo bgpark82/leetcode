b880fdf4862171504297968ae6853ed45a7b99e9
Byeonggil Park <bgpark82@gmail.com>
2023-07-16 15:04:09 +0900

    Time: 759 ms (8.27%), Space: 50.7 MB (77.33%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // if we sort it or not : O(nlogn)
+        // brute force : O(n^4logn)
+        
+        // two sum + 1
+        // -4 -1 -1 0 1 2
+        //    i     l r  
+        // -4 -1 2 = -3 < 0
+        // -4 0  2 = -2 < 0
+        // -4 1  2 = -1 < 0
+        // -1 -1 2 = 0 == 0  add to list
+        // -1. 0 1 = 0 == 0  add to list (process has to go on)
+        
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet();
+        List<List<Integer>> ans = new ArrayList();
+        
+        for(int i = 0; i < nums.length - 1; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    List<Integer> tmp = Arrays.asList(nums[i], nums[l], nums[r]);
+                    Collections.sort(tmp);
+                    set.add(tmp);    
+                    r--; l++;
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    r--;                    
+                } else {
+                    l++;
+                }
+            }
+
+        }
+        ans.addAll(set);
+        return ans;
+        
+        
+    }
+}

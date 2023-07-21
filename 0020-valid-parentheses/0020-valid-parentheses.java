2ca9e8e26ca1406fac762a6cecf99c0c9053300d
Byeonggil Park <bgpark82@gmail.com>
2023-07-21 23:16:41 +0900

    Time: 3 ms (15.86%), Space: 40.7 MB (52.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        
-        ArrayDeque<Character> stack = new ArrayDeque();
+        Stack<Character> stack = new Stack(); // O(n)
         Map<Character, Character> map = new HashMap();
-        map.put('[', ']');
         map.put('(', ')');
+        map.put('[', ']');
         map.put('{', '}');
         
+        if(s.length() == 1 || s.length() % 2 == 1) return false;
+        
+        // Time Complexity: O(N)
+        // Space Complexity: O(N) 
         for(char c : s.toCharArray()) {
+            // just pop if it match
             if(!stack.isEmpty() && map.containsKey(stack.peek()) && map.get(stack.peek()) == c) {
                 stack.pop();
             } else {
+                // open bracket
                 stack.push(c);    
             }
-        }   
-
+        }
+        
         return stack.isEmpty();
     }
 }

7ea5eb0b986d27a55c9aceefef2a7a6526e618dd
Peter Park <bgpark82@gmail.com>
2023-08-15 14:24:14 +0900

    Time: 1 ms (100.00%), Space: 43.5 MB (85.71%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -15,10 +15,12 @@ class Solution {
     
     private void backtracking(int[] nums, int index, List<Integer> set, List<List<Integer>> ans) {
         
+        // 1 2 2
+        
         ans.add(new ArrayList(set));
         
         for(int i = index; i < nums.length; i++) {
-            // skip?
+            // skip? backtracking으로 추가되는 다음 인덱스와 for loopd에서 추가되는 다음 인덱스가 중복된다
             if(i > index && nums[i] == nums[i-1]) continue;
             set.add(nums[i]);
             backtracking(nums, i + 1, set, ans);

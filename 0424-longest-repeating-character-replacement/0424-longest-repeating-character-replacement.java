ede23f6b3a59689e560a4710f7ae99a4bb13ffbd
Peter Park <bgpark82@gmail.com>
2023-08-19 20:24:37 +0900

    Time: 10 ms (61.48%), Space: 41.3 MB (89.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -15,9 +15,9 @@ class Solution {
         for(int r = 0, l = 0; r < s.length(); r++) {
             // 꾸준히 문자 등장 횟수를 저장한다
             arr[s.charAt(r) - 'A']++;
-            
-            
-            max = Math.max(max, arr[s.charAt(r) - 'A']); // max: window 안에서 최대 맥스값, 실제로 문자를 바꿀 필요없이, 바꿔야 할 문자의 개수를 저장하면 된다 
+            // 68.75 60.59
+
+            max = findMax(arr); // max: window 안에서 최대 맥스값, 실제로 문자를 바꿀 필요없이, 바꿔야 할 문자의 개수를 저장하면 된다 
             // l을 증가시킬 조건
             if (r - l + 1 - max > k) { // r-l+1 : 현재 윈도우, r-l+1-max : 현재 window에서 
                 arr[s.charAt(l) - 'A']--;

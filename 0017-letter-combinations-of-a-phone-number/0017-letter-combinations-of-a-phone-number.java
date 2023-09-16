39fffb2a69a61c4ccab0b24be8804dd114378133
Peter Park <bgpark82@gmail.com>
2023-09-16 23:22:21 +0900

    Time: 5 ms (48.46%), Space: 41.5 MB (26.82%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,32 +4,21 @@ class Solution {
     
     public List<String> letterCombinations(String digits) {
         
-        
 		LinkedList<String> ans = new LinkedList<String>();
         if(digits.isEmpty()) return ans;
 		
 		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 		ans.add("");
         
-        // 2, 3
 		for(int i = 0; i< digits.length(); i++){
 			int x = Character.getNumericValue(digits.charAt(i));
             
-            // 자리수 별로
-            // 
-            // a b c
-            // c -> cd, ce, af
 			while(ans.peek().length() == i){
 				String t = ans.remove();
-                System.out.println("t=" + t);
-                
-                // a, b, c
-                // ad, ae, af
+            
 				for(char s : mapping[x].toCharArray()) {
-                    String temp = t + s;
-                    System.out.println(temp);
-					ans.add(temp);
-                    }
+					ans.add(t + s);
+                }
 			}
 		}
 		return ans;

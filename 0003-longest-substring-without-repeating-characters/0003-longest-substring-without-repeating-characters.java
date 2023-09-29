fb044464a6bc0004fdb01af0c739376f7b725c7e
Peter Park <bgpark82@gmail.com>
2023-09-29 17:49:15 +0900

    Time: 6 ms (66.25%), Space: 44 MB (23.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -24,23 +24,58 @@ class Solution {
         
         // set
         // r s v b  
-        
         int max = 0;
         Set<Character> set = new HashSet();
+        // abc, bca
         int l = 0, r = 0;
+        
+        // qrsvbspk
+        //    l
+        //      r
+        // svb
+        
+        
         while(r < s.length()) {
-            if(set.contains(s.charAt(r))) {
+            // 만약 r 포인터의 문자값이
+            // set에 있는 경우
+            // l은 r을 찾을 때까지 계속 오른쪽으로 포인터를 이동해야 한다
+            if(!set.contains(s.charAt(r))) {
+                set.add(s.charAt(r));
+                max = Math.max(max, set.size());
+                r++;
+            } else {
+                // r 포인터의 문자값을 찾을 때까지 l 포인터를 이동하면서 제거
+                // l 포인터의 문자값이 r 포인터의 문자값과 일치하면서
+                // r 포인터가 다음 루프에서 진행할 수 있다
                 set.remove(s.charAt(l));
                 l++;
-            } else {
-                set.add(s.charAt(r));
-                r++;
-                max = Math.max(max, set.size());    
             }
-            
         }
         
         return max;
+        
+        
+        
+        
+        
+        
+        
+        
+//         int max = 0;
+//         Set<Character> set = new HashSet();
+//         int l = 0, r = 0;
+//         while(r < s.length()) {
+//             if(set.contains(s.charAt(r))) {
+//                 set.remove(s.charAt(l));
+//                 l++;
+//             } else {
+//                 set.add(s.charAt(r));
+//                 r++;
+//                 max = Math.max(max, set.size());    
+//             }
+//         }
+        
+//         return max;
         
         
         

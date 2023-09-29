710f2bd272c2ff8a0581cc4310cdb50d6374e192
Peter Park <bgpark82@gmail.com>
2023-09-29 13:00:09 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (24.80%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,24 +15,60 @@
  */
 class Solution {
     
-    // 일단 답을 저장하고, traversal하는게 많은 듯
+    
     private boolean ans = true;
     
     public boolean isBalanced(TreeNode root) {
-        height(root);
+        dfs(root);
+        
         return ans;
     }
     
-    // traversal의 결과가 중요하지는 않다. 과정 중에 답을 찾으면 그만
-    private int height(TreeNode node) {
-        if (node == null) return 0;
+    private int dfs(TreeNode node) {
+        // 1
+        // n.2
+        //. n 3
         
-        int l = height(node.left);
-        int r = height(node.right);
+        
+        if(node == null) return 0;
+        
+        int l = dfs(node.left);
+        int r = dfs(node.right);
         
         if(Math.abs(l - r) > 1) ans = false;
         
-        return Math.max(l, r) + 1;
-        
+        return Math.max(l,r) + 1;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // 일단 답을 저장하고, traversal하는게 많은 듯
+//     private boolean ans = true;
+    
+//     public boolean isBalanced(TreeNode root) {
+//         height(root);
+//         return ans;
+//     }
+    
+//     // traversal의 결과가 중요하지는 않다. 과정 중에 답을 찾으면 그만
+//     private int height(TreeNode node) {
+//         if (node == null) return 0;
+        
+//         int l = height(node.left);
+//         int r = height(node.right);
+        
+//         if(Math.abs(l - r) > 1) ans = false;
+        
+//         return Math.max(l, r) + 1;
+        
+    // }
 }

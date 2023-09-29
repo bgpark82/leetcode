99b3fc76ad0c9bb6d180e08cc603cf35345ee8b8
Peter Park <bgpark82@gmail.com>
2023-09-29 12:23:41 +0900

    Time: 1 ms (42.66%), Space: 44 MB (64.29%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,46 +14,57 @@
  * }
  */
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-     
-        // 1
-        //    3
-        //  1.  4
-        //.   2
-        
-        // first solution
-        // iterate bst
-        // save treemap
-        // return nth value
-        // nlogn
-        
-        Set<Integer> set = new TreeSet();
-        
     
-        Deque<TreeNode> queue = new ArrayDeque();
-        queue.addLast(root);
-        
-        while(!queue.isEmpty()) {
-            
-            TreeNode node = queue.removeFirst();
-            
-            set.add(node.val);
-            
-            if(node.left != null) queue.addLast(node.left);
-            if(node.right != null) queue.addLast(node.right);
-        }
-        
-        for(Integer s : set) {
-            System.out.print(s + " ");
-        }
+    private List<Integer> list = new ArrayList();
+    
+    public int kthSmallest(TreeNode root, int k) {        
+        // inorder 순서대로
+        // 1 2 3 4
+        dfs(root);
         
         int i = 1;
-        for(Integer s : set) {
-            if(i++ == k) {
-                return s;
-            }
+        for(int l : list) {
+            if(i++ == k) return l;
         }
         return -1;
         
     }
+    
+    private void dfs(TreeNode node) {
+        if(node == null) return;
+        
+        dfs(node.left);
+        list.add(node.val);
+        dfs(node.right);
+    }
+    
+//     public int kthSmallest(TreeNode root, int k) {
+        
+//         Set<Integer> set = new TreeSet();
+    
+//         Deque<TreeNode> queue = new ArrayDeque();
+//         queue.addLast(root);
+        
+//         while(!queue.isEmpty()) {
+            
+//             TreeNode node = queue.removeFirst();
+            
+//             set.add(node.val);
+            
+//             if(node.left != null) queue.addLast(node.left);
+//             if(node.right != null) queue.addLast(node.right);
+//         }
+        
+//         for(Integer s : set) {
+//             System.out.print(s + " ");
+//         }
+        
+//         int i = 1;
+//         for(Integer s : set) {
+//             if(i++ == k) {
+//                 return s;
+//             }
+//         }
+//         return -1;
+//     }
 }

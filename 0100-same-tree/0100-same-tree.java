4ef0f7cf7e4592b9eaca08594dae46b0f5119e6c
Peter Park <bgpark82@gmail.com>
2023-09-29 13:05:25 +0900

    Time: 0 ms (100.00%), Space: 39.8 MB (50.91%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -19,9 +19,10 @@ class Solution {
         // 1.   1
         //2.3  2 3
         if(p == null && q == null) return true;
-        if(p == null && q != null) return false;
-        if(p != null && q == null) return false;
-        if(p.val != q.val) return false;
+        
+        if(p == null && q != null ||
+           p != null && q == null ||
+            p.val != q.val) return false;
         
         
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);

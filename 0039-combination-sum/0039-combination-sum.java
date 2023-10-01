b8a11e40e7d2213387642b387c497aa7e912d70a
Peter Park <bgpark82@gmail.com>
2023-10-01 17:57:29 +0900

    Time: 2 ms (74.86%), Space: 43.7 MB (64.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
         if(candidates == null || candidates.length == 0) return new ArrayList();
         
-        List<Integer> set = new ArrayList();
         List<List<Integer>> ans = new ArrayList();
+        List<Integer> step = new ArrayList();
         
-        dfs(candidates, target, 0, set, ans, 0);
+        bt(candidates, 0, step, ans, target, 0);
         
         return ans;
     }
     
-    private void dfs(int[] candidates, int target, int sum, List<Integer> set, List<List<Integer>> ans, int index) {
-        // 8 7 4 3
-        // 4 4 
-        if (sum > target) return;
-        else if(sum == target) {
-            ans.add(new ArrayList(set));
-        } else {
-            for(int i = index; i < candidates.length; i++) {
-                set.add(candidates[i]);
-                dfs(candidates, target, sum + candidates[i], set, ans, i);
-                set.remove(set.size() - 1);
-            }            
+    private void bt(int[] candidates, int index, List<Integer> step, List<List<Integer>> ans, int target, int sum) {
+
+        if(sum > target) return;
+        
+        if(sum == target) {
+            ans.add(new ArrayList(step));
+            return;
+        }
+        
+        for(int i = index; i < candidates.length; i++) {
+            step.add(candidates[i]);
+            bt(candidates, i, step, ans, target, sum + candidates[i]);
+            step.remove(step.size() - 1);
         }
     }
+    
 }

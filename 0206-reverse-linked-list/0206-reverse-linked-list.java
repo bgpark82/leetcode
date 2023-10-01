b6af2f95faffd297c93700a4ffe88c5917bae296
Peter Park <bgpark82@gmail.com>
2023-10-01 19:33:37 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (75.48%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,25 +10,22 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // 1 -> 2 -> 3 -> 4 -> 5
-        // c c
-        // p
+        // n 1 2 3 4 5
+        //     c
+        //   p
+        // c.next = pre
         
-        
-        // 1 2 3 4 5 6
         
         return dfs(head, null);        
     }
     
-    // null 1 2 3 4 5
-    // p    c
-    //.     p c
+    // n 1 2 3 4 5
+    //           c
+    //         p
     ListNode dfs(ListNode curr, ListNode pre) {
-        // 마지막 노드면 이전 노드로 이동
-        if (curr == null) return pre;
-        // 다음 노드를 현재 노드로 변경
+        if(curr == null) return pre;
+        
         ListNode next = curr.next;
-        // 현재 노드의 다음 노드를 pre로 변경
         curr.next = pre;
         return dfs(next, curr);
     }

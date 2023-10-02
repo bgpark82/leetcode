d10692d605c698fd1055547077b4446e48f3d6f6
Peter Park <bgpark82@gmail.com>
2023-10-02 15:24:47 +0900

    Time: 18 ms (18.79%), Space: 47.8 MB (14.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,23 +3,22 @@ class Solution {
         // SC : O(n)
         // TC : O(n^2 * log(n))
         Map<String, List<String>> map = new HashMap();
-
-        for (String str : strs) {
-            // nat
-            // 문자 정렬의 시간복잡도(O(nlogn))을 공간 복잡도(O(n))와 시간복잡도(O(n))으로 치환가능
+        
+        for(String str : strs) {
+            
             int[] hash = new int[26];
             for(char c : str.toCharArray()) {
                 hash[c - 'a']++;
             }
-            String nc = Arrays.toString(hash);
-            // ant
-            map.putIfAbsent(nc, new ArrayList());
-            map.get(nc).add(str);
+            String key = Arrays.toString(hash);
+            
+            map.putIfAbsent(key, new ArrayList());
+            map.get(key).add(str);
         }
-
+        
         List<List<String>> ans = new ArrayList();
         ans.addAll(map.values());
-
+        
         return ans;
     }
 }

3ba4f33775657712130999358fab55b5e79eb5d3
Peter Park <bgpark82@gmail.com>
2023-10-02 14:47:31 +0900

    Time: 22 ms (91.92%), Space: 59.5 MB (13.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -2,21 +2,23 @@ class Solution {
     public int[] dailyTemperatures(int[] temp) {
 
         // 73 74 75 71 69 72 76 73
-        //    c
-        // s
-        // 1
+        //    c                        
+        // s                        history of indices in ordre
+        // 1  1  4  2  1  1
+        
+        
+        // 우리가 원하는 것은 날짜의 차이이다 (날짜 = index)
+        // sliding window + stack
         
         int[] ans = new int[temp.length];
-        Stack<Integer> s = new Stack();
+        Deque<Integer> stack = new ArrayDeque();
         
-        for(int curr = 0; curr < temp.length; curr++) {
-            
-            while(!s.isEmpty() && temp[curr] > temp[s.peek()]) {
-                int prev = s.pop();
-                ans[prev] = curr - prev;
+        for(int cur = 0; cur < temp.length; cur++) {
+            while(!stack.isEmpty() && temp[cur] > temp[stack.peek()]) {
+                int prev = stack.removeFirst();
+                ans[prev] = cur - prev;
             }
-            
-            s.push(curr);
+            stack.addFirst(cur);
         }
         
         return ans;

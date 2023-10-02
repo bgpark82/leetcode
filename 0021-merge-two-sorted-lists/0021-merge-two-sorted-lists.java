76e5e5ad95b1a42c820b7d07c5d74b9ea4937d5e
Peter Park <bgpark82@gmail.com>
2023-10-02 10:08:29 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (19.11%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -23,24 +23,29 @@ class Solution {
         while(p1 != null && p2 != null) {
             if(p1.val <= p2.val) {
                 head.next = p1;
-                head = head.next;
                 p1 = p1.next;
             } else {
                 head.next = p2;
-                head = head.next;
                 p2 = p2.next;
             }
-        }
-        while(p1 != null) {
-            head.next = p1;
             head = head.next;
-            p1 = p1.next;
         }
-        while(p2 != null) {
-            head.next = p2;
-            head = head.next;
-            p2 = p2.next;
-        }
+        
+        // 아래 while문 2개와 아래 코드는 똑같다
+        // 즉, p1이 null이면 p2는 아직 다 돌지 못한 상태이다
+        // p1과 p2는 순서대로 정렬되어 있으므로 head에 연결시키기만 하면 된다
+        head.next = p1 == null ? p2 : p1;
+        
+//         while(p1 != null) {
+//             head.next = p1;
+//             head = head.next;
+//             p1 = p1.next;
+//         }
+//         while(p2 != null) {
+//             head.next = p2;
+//             head = head.next;
+//             p2 = p2.next;
+//         }
         
         
         return ans.next;

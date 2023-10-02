db3d1905791d71780fadb14d22f410b5705e6826
Peter Park <bgpark82@gmail.com>
2023-10-02 14:30:38 +0900

    Time: 60 ms (60.46%), Space: 55.4 MB (61.28%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
 
-        // 73,74,75,71,69,72,76,73
-        // 0 1 2 3 4 5 6 7 8
-        // curr : 7
-        // s : 6 7
-        //     
-        // a : 1, 1, 4, 2, 1, 1
+        // 73 74 75 71 69 72 76 73
+        //    c
+        // s
+        // 1
         
         int[] ans = new int[temp.length];
         Stack<Integer> s = new Stack();

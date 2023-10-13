23f479a74afd3386edd8a750e62ddf3da5f8f65d
Peter Park <bgpark82@gmail.com>
2023-10-13 10:15:44 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (89.57%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,24 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         
-        // trim 내부 구현??
-        // s = s.trim();
-        
-        int len = s.length();
-        int r = len - 1; 
+        int len = 0, r = s.length() - 1; 
         while(r >= 0 && s.charAt(r) == ' ') r--;
-        int l = r;
-        while(l >= 0 && s.charAt(l) != ' ') l--; 
-
-        return r - l;
-        
-        
-        // int i = s.length() - 1;
-        // for(; i >= 0; i--) {
-        //     if(s.charAt(i) == ' ') break;
-        // }
-        // return s.length() - i - 1;
-        // String[] arr = s.trim().split(" ");
-        // return arr[arr.length - 1].length();
+        while(r >= 0 && s.charAt(r) != ' ') {
+            r--;
+            len++;
+        } 
+        return len;
     }
 }

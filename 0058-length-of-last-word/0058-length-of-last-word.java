d9b3da8b435d2d4092a36d5764515204f8113b1a
Peter Park <bgpark82@gmail.com>
2023-10-13 09:13:23 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (54.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-
-        String[] arr = s.trim().split(" ");
-        return arr[arr.length - 1].length();
+        
+        s = s.trim();
+        
+        int i = s.length() - 1;
+        for(; i >= 0; i--) {
+            if(s.charAt(i) == ' ') break;
+        }
+        return s.length() - i - 1;
+        // String[] arr = s.trim().split(" ");
+        // return arr[arr.length - 1].length();
     }
 }

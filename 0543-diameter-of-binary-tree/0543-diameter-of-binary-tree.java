e3e111257c0057b2a52b5c82ecc9bb411f0c3fee
Peter Park <bgpark82@gmail.com>
2023-10-14 20:22:41 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (68.05%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -23,14 +23,40 @@ class Solution {
     }
     
     private int dfs(TreeNode node) {
-        
         if(node == null) return 0;
         
+        // 4인 경우, l=0,r=0
+        // 2인 경우, l=1,r=1
+        // 1인 경우, l=2,r=1
         int left = dfs(node.left);
         int right = dfs(node.right);
         
+        // 현재 노드에서 왼쪽과 오른쪽의 길이의 합이 최대치인 것
         diameter = Math.max(diameter, left + right);
         
+        // 부모 노드로 올라갈때 왼쪽과 오른쪽 중 가장 긴 값을 선택한다
         return Math.max(left, right) + 1;
     }
+    
+    
+//     // what to find
+//     private int diameter = 0;
+    
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         dfs(root);
+//         return diameter;
+//     }
+    
+//     // each node (snapshot)
+//     private int dfs(TreeNode node) {
+//         if(node == null) return 0;
+        
+//         int left = dfs(node.left);
+//         int right = dfs(node.right);
+        
+//         // update target value in every traversal
+//         diameter = Math.max(diameter, left + right);
+        
+//         return Math.max(left, right) + 1;
+//     }
 }

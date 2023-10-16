6af36f3aa51a19341de5c99b57aa897d74ce3c5b
Peter Park <bgpark82@gmail.com>
2023-10-16 23:42:05 +0900

    Time: 0 ms (100.00%), Space: 47.9 MB (99.04%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,37 +15,36 @@
  */
 class Solution {
     
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) return 0;
+        if(root.val < low) return rangeSumBST(root.right, low, high);
+        if(root.val > high) return rangeSumBST(root.left, low, high);
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+    
+    // 10 + 7 + 15
+    
+    
+//     int sum = 0;
+    
 //     public int rangeSumBST(TreeNode root, int low, int high) {
-            
-//         if(root == null) return 0;
-        
-        
-//         if(root.val < low) return rangeSumBST(root.right, low, high);
-        
-//         // 현재 노드가 low보다 작지 않고 high보다 작으면
+//         // 7 ~ 15
+//         //   10    equal -> sum
+//         //  5      less -> right
+//         //.   7.   eqaul -> sum
+//         dfs(root, low, high);
+//         return sum;
 //     }
     
-    
-    int sum = 0;
-    
-    public int rangeSumBST(TreeNode root, int low, int high) {
-        // 7 ~ 15
-        //   10    equal -> sum
-        //  5      less -> right
-        //.   7.   eqaul -> sum
-        dfs(root, low, high);
-        return sum;
-    }
-    
-    private void dfs(TreeNode node, int low, int high) {
-        if(node == null) return;
+//     private void dfs(TreeNode node, int low, int high) {
+//         if(node == null) return;
         
-        if(node.val < low) dfs(node.right, low, high);
-        else if(node.val > high) dfs(node.left, low, high);
-        else {
-            sum += node.val;
-            dfs(node.left, low, high);
-            dfs(node.right, low, high);
-        }
-    }
+//         if(node.val < low) dfs(node.right, low, high);
+//         else if(node.val > high) dfs(node.left, low, high);
+//         else {
+//             sum += node.val;
+//             dfs(node.left, low, high);
+//             dfs(node.right, low, high);
+//         }
+//     }
 }

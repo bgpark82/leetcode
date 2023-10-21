baa8264f74eace5c8693c8a8ace222d15ba819c3
Peter Park <bgpark82@gmail.com>
2023-10-21 18:45:14 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (91.16%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,37 +1,47 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
+        
+        // a b c d e f ..
+        // 2 3 4 5
+        
+        // prev : leet
+        //           0
+        // curr : leetcode
+        //           1
         //
+        // hell o
+        // 0622 14
+        // leet code
+        // 166195
         
-        // hello -> h e l l o
-        //          0 6 2 2 
-        // leetcode -> l e e t c o d e
-        //.            2 6 6
+        int[] map = new int[26];
+        for(int i = 0; i < 26; i++) {
+            map[order.charAt(i) - 'a'] = i;
+        }
         
-        // 순서에 대한 정보를 저장
-        // word
-        // world
-        // row
-        if(words == null || words.length == 0) return false;
-        if(words.length == 1) return true;
         
         for(int i = 1; i < words.length; i++) {
-            String curr = words[i];
-            String prev = words[i-1];
+            String prev = words[i-1];        
+            String curr = words[i];        
             
-            if(!check(order, curr, prev)) return false;
-        }   
+            // for(int j = 0; j < len; j++) {
+            //     if(map[prev.charAt(j) - 'a'] < map[curr.charAt(j) - 'a']) break;
+            //     if(map[prev.charAt(j) - 'a'] > map[curr.charAt(j) - 'a']) return false;
+            // }
+            
+            if(!check(map, prev, curr)) return false;
+        }
+        
         return true;
     }
     
-    private boolean check(String order, String curr, String prev) {
-        int clen = curr.length(), plen = prev.length();
-        int len = Math.min(plen, clen);
-        for(int i = 0; i < len; i++) {
-            if(curr.charAt(i) != prev.charAt(i)) {
-                return order.indexOf(curr.charAt(i)) > order.indexOf(prev.charAt(i));
-            }
+    private boolean check(int[] map, String prev, String curr) {
+        int len = Math.min(prev.length(), curr.length());
+        for(int j = 0; j < len; j++) {
+            if(map[prev.charAt(j) - 'a'] < map[curr.charAt(j) - 'a']) return true;
+            if(map[prev.charAt(j) - 'a'] > map[curr.charAt(j) - 'a']) return false;
         }
-        // 같은 경우 clen 길이가 긴 것
-        return clen >= plen;
+        return prev.length() <= curr.length();
     }
+    
 }

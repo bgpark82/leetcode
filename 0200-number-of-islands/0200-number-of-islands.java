986dd0a12a496655c170f38fc58919c1108d103b
Peter Park <bgpark82@gmail.com>
2023-10-21 19:00:29 +0900

    Time: 3 ms (71.53%), Space: 47.7 MB (38.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,20 +1,13 @@
 class Solution {
     
-    private boolean[][] check;
-    private int[] dx = new int[] {-1, 0, 1, 0};
-    private int[] dy = new int[] {0, -1, 0, 1};
-    private int count = 0;
-    
+    int count = 0;
     
     public int numIslands(char[][] grid) {
         
-        
-        check = new boolean[grid.length][grid[0].length];
-        
         for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[i].length; j++) {
-                if(!check[i][j] && grid[i][j] == '1') {
-                    bfs(grid, i, j);
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    dfs(grid, i, j);
                     count++;
                 }
             }
@@ -24,36 +17,13 @@ class Solution {
     }
     
     private void dfs(char[][] grid, int x, int y) {
-        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || check[x][y] || grid[x][y] == '0') return;
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '0') return;
         
-        check[x][y] = true;
+        grid[x][y] = '0';
         
         dfs(grid, x + 1, y);
         dfs(grid, x - 1, y);
         dfs(grid, x, y + 1);
         dfs(grid, x, y - 1);
-    }
-    
-    
-    private void bfs(char[][] grid, int i, int j) {
-        
-        Deque<int[]> queue = new ArrayDeque();
-        queue.addLast(new int[] {i, j});
-        
-        while(!queue.isEmpty()) {
-            
-            int[] pos = queue.removeFirst();
-            check[i][j] = true;
-            
-            for(int k = 0; k < 4; k++) {
-                int x = pos[0] + dx[k];
-                int y = pos[1] + dy[k];
-                
-                if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || check[x][y] || grid[x][y] == '0') continue;
-
-                queue.addLast(new int[] {x, y});
-                check[x][y] = true;
-            }
-        }
     }
 }

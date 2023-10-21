24094d6189466ec8e814a0ced0989d14d973af10
Peter Park <bgpark82@gmail.com>
2023-10-21 18:10:12 +0900

    Time: 0 ms (100.00%), Space: 48.6 MB (58.41%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,14 +15,28 @@
  */
 class Solution {
     
+    //.        10
+    //.    5.        15
+    //. 3.    7.          18
+    //1.  4      8. 
+    // 8 ~ 18
+    
+    // 1. if it is 5, we don't need to move to left at all
+    //        10
+    //    5        15
+    //       7          18
+    //         8. 
+    
+    // 10 + 8 + 15 + 18
+    
+    
     // return sum of value of node ranging between low and hight (inclusive)
     // sum of left and right is the answer
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null) return 0; // if there is no value nothing to sum
         
         // these branches out in specific condition
-        // if value of node is less than low, move to node right and get answer (sum) of it
-        if(root.val < low) return rangeSumBST(root.right, low, high);
+        if(root.val < low) return rangeSumBST(root.right, low, high); // 1. don't need to move to left at all
         if(root.val > high) return  rangeSumBST(root.left, low, high);
         // if it within range
         return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);

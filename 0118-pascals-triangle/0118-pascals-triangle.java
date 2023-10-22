3ddc41b86c7fe31d0b2a715e57671bc99686abdc
Peter Park <bgpark82@gmail.com>
2023-10-22 11:46:14 +0900

    Time: 1 ms (92.54%), Space: 41 MB (44.41%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -7,19 +7,21 @@ class Solution {
         // 5 14641   01234   0 01 12 23 3
         
         List<List<Integer>> list = new ArrayList();
-        List<Integer> first = new ArrayList();
-        first.add(1);
-        list.add(first);
+        // List<Integer> first = new ArrayList();
+        // first.add(1);
+        // list.add(first);
         
         List<Integer> prev = null;
         // 1, 2, 3, 4, 5
-        for(int size = 1; size < numRows; size++) {
-            prev= list.get(size-1);
+        for(int size = 1; size <= numRows; size++) {
+            // prev= list.get(size-1);
             List<Integer> l = new ArrayList();
-            for(int j = 0; j <= size; j++) {
-                int n1 = (j <= 0) ? 0 : prev.get(j - 1);
-                int n2 = (j >= prev.size()) ? 0 : prev.get(j);
-                l.add(n1 + n2);
+            for(int j = 0; j <= size - 1; j++) {
+                if(j == 0 || j == size - 1) l.add(1);
+                else l.add(list.get(size-2).get(j-1) + list.get(size-2).get(j));
+                // int n1 = (j <= 0) ? 0 : prev.get(j - 1);
+                // int n2 = (j >= prev.size()) ? 0 : prev.get(j);
+                // l.add(n1 + n2);
             }
             list.add(l);
         }

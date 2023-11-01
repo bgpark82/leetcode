e94e5bb4f09ac26b00898c47e999cafba7a9adae
Peter Park <bgpark82@gmail.com>
2023-11-01 23:41:45 +0900

    Time: 34 ms (14.06%), Space: 44.7 MB (21.10%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Pair> stack = new Stack();
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') { 
+                stack.push(new Pair(c, i));
+            } else if(c == ')') {
+                if(!stack.isEmpty() && stack.peek().key == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(new Pair(c, i));
+                }
+            } 
+        }
+        
+        while(!stack.isEmpty()) {
+            Pair p = stack.peek();
+            System.out.println(p.key + " " + p.index );
+            sb.deleteCharAt(stack.pop().index);
+        }
+        
+        return sb.toString();       
+    }
+    
+    class Pair {
+        char key;
+        int index;
+        
+        public Pair(char key, int index) {
+            this.key = key;
+            this.index = index;
+        }
+    }
+}

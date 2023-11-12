2a798861fad00894a7ba2bd352f3961f1f99777a
Peter Park <bgpark82@gmail.com>
2023-11-12 13:48:22 +0900

    Time: 42 ms (48.75%), Space: 43.2 MB (47.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -12,13 +12,13 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
         
-        for(int i = nums.length - 1; i >= 0; i--) {
-            for(int j = i + 1; j < nums.length; j++) {
+        for(int i = len - 1; i >= 0; i--) {
+            for(int j = i + 1; j < len; j++) {
                 if(nums[i] >= nums[j]) continue;
                 dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
         Arrays.sort(dp);
-        return dp[dp.length - 1];
+        return dp[len - 1];
     }
 }

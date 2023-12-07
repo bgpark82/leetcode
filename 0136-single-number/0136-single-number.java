4a36a5475f40262d5f3ec3b183df4d84faee6904
Peter Park <bgpark82@gmail.com>
2023-12-07 23:47:11 +0900

    Time: 1 ms (99.93%), Space: 45 MB (17.96%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,15 +3,19 @@ class Solution {
         // TC : O(n)
         // SC : O(1)
         
-        Map<Integer, Integer> map = new HashMap();
-        
-        for(int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        // Bit Manipulation으로 표현하는 방법을 알아야 한다
+        // 
+        // XOR은 bit의 각 자리수를 비교해서 같으면 0, 다르면 1
+        // 4 100
+        // 1 001
+        // 2 010
+        // 1 001
+        // 2 010
+        // 
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            ans ^= nums[i];
         }
-        
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if(entry.getValue() == 1) return entry.getKey();
-        }
-        return -1;
+        return ans;
     }
 }

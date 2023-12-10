778fcef810be1e3809894221bc2201ff18de76a8
Peter Park <bgpark82@gmail.com>
2023-12-10 14:32:10 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (73.97%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -22,11 +22,15 @@ class Solution {
         //           f        
         //       s
         
+        // LinkedList는 constant space를 쓰기 위해 사용한다
+        // 데이터 구조에 대한 이해가 많이 없는게 아닐까...?
+        // head는 움직여야 되므로 어딘가 저장해야 한다
         
-        // 시작값을 -1에서 시작
-        ListNode ans = new ListNode(0, head);
         
-        ListNode fast = head, slow = ans;
+        // 시작값을 -1에서 시작, 더미를 잘 써야 한다. 더미는 head와 별개로 동작하기 위해서 별로도 iterate한다 (캐싱)
+        ListNode dummy = new ListNode(0, head);
+        
+        ListNode fast = head, slow = dummy;
         while(n-- > 0) fast = fast.next;
         while(fast != null) {
             fast = fast.next;
@@ -34,6 +38,6 @@ class Solution {
         }
         slow.next = slow.next.next;
         
-        return ans.next;
+        return dummy.next;
     }
 }

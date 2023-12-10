63fb350c0971d23f40f192bc4e5f74d65feba367
Peter Park <bgpark82@gmail.com>
2023-12-10 14:37:18 +0900

    Time: 0 ms (100.00%), Space: 41 MB (14.27%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -26,8 +26,11 @@ class Solution {
         // 데이터 구조에 대한 이해가 많이 없는게 아닐까...?
         // head는 움직여야 되므로 어딘가 저장해야 한다
         
-        
+        // linkedlist는 공간 복잡도를 줄이기 위해 사용한다. list 등으로 캐싱을 하지 않는다
+        // fast, slow 노드 사용하거나 reverse는 3개의 node를 사용한다
+        // 결국, constant space를 쓰기 위함이다
         // 시작값을 -1에서 시작, 더미를 잘 써야 한다. 더미는 head와 별개로 동작하기 위해서 별로도 iterate한다 (캐싱)
+        // 타겟 노드를 찾기 위한 2개의 포인터를 사용한다
         ListNode dummy = new ListNode(0, head);
         
         ListNode fast = head, slow = dummy;

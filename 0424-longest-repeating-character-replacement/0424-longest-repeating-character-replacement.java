f2d1c9ab42b296db78fac587a72762934cc54b79
Peter Park <bgpark82@gmail.com>
2024-01-14 16:28:26 +0900

    Time: 6 ms (94.02%), Space: 42.3 MB (61.74%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,13 +1,14 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // AABABBA
-        //   l
-        //       r
-        // AB
-        // 13
-        // m 3
-        // a 4
         
+        // k = 2
+        // AABABBA
+        // l
+        //     r
+        //
+        // max = 3
+        // AB
+        // 33
         int[] arr = new int[26];
         int max = 0, ans = 0;
         
@@ -25,58 +26,6 @@ class Solution {
             ans = Math.max(ans, r - l + 1);
         }
         return ans;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int[] arr = new int[26];
-//         int ans = 0, max = 0;
-//         for(int r = 0, l = 0; r < s.length(); r++) {
-//             // 꾸준히 문자 등장 횟수를 저장한다
-//             arr[s.charAt(r) - 'A']++;
 
-//             // l을 증가시킬 조건
-//             if (r - l + 1 - findMax(arr) > k) {
-//                 arr[s.charAt(l) - 'A']--;
-//                 l++;
-//             }
-            
-//             // 꾸준히 최대값을 계산한다
-//             ans = Math.max(ans, r - l + 1);
-//         }
-        
-//         return ans;
-    }
-    
-    // window 내에서 가장 많은 개수를 가진 캐릭터의 개수
-    private int findMax(int[] arr) {
-        int max = 0;
-        for(int i = 0; i < arr.length; i++) {
-            max = Math.max(max, arr[i]);
-        }
-        return max;
     }
 }

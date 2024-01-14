84bd3a61b3f8808abce346f32c348f7fd4884a01
Peter Park <bgpark82@gmail.com>
2024-01-14 23:09:37 +0900

    Time: 1 ms (73.30%), Space: 42.9 MB (8.28%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,23 @@
 class Solution {
+    
     public List<List<Integer>> subsets(int[] nums) {
-        
-        // 1,2,3
-        // i
-        // [], [1]. [2]. [3]. [1,2] [1,3] [2,3] [1,2,3]
-        
-        
-        // Backtracking template
-        if (nums == null || nums.length == 0) return new ArrayList();
-        
+        // 1 2 3
         List<List<Integer>> ans = new ArrayList();
         List<Integer> step = new ArrayList();
+        
         bt(nums, 0, step, ans);
+        
         return ans;
     }
     
-    /**
-     * @param nums: input array
-     * @param start: newly assigned start index
-     * @param step: combination of input array at index start
-     * @param ans: answer
-     */
-    private void bt(int[] nums, int start, List<Integer> step, List<List<Integer>> ans) {
+    
+    private void bt(int[] nums, int idx, List<Integer> step, List<List<Integer>> ans) {
+        // [] [1] [1,2] [1,2,3] 
+        
         ans.add(new ArrayList(step));
-        for(int i = start; i < nums.length; i++) {
-            step.add(nums[i]);
+        
+        for(int i = idx; i < nums.length; i++) {
+            step.add(nums[i]); 
             bt(nums, i + 1, step, ans);
             step.remove(step.size() - 1);
         }

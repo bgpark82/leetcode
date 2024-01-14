cc834bdfd863c8111f6abcc64aadf8cf9a4a20f6
Peter Park <bgpark82@gmail.com>
2024-01-14 12:25:55 +0900

    Time: 0 ms (100.00%), Space: 44.6 MB (12.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
-        ListNode slow = head, fast = head;
+        // 1 2 3 4  
+        //     s
+        //         f  
+        ListNode fast = head;
+        ListNode slow = head;
         
         while(fast != null && fast.next != null) {
-            slow = slow.next;;
             fast = fast.next.next;
-            if(slow == fast) return true;
+            slow = slow.next;
+            if(fast == slow) return true;
         }
         return false;
     }

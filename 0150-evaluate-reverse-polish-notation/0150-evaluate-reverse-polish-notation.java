41baf708aa85e3c7b3a70e5d47fdff49c586b9f2
Peter Park <bgpark82@gmail.com>
2024-01-15 23:18:30 +0900

    Time: 5 ms (97.10%), Space: 44.4 MB (44.17%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,37 +1,27 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        // 10 6 9 3 +
-        // 10 6 12 -11 *
-        // 10 6 -132 /
-        // 10 0 *
-        // 0 17 + 
-        // 17 5 + 
-        // 22
-        
-        Stack<Integer> s = new Stack();
-        
-        
+        Stack<Integer> stack = new Stack();
         for(String token : tokens) {
-            // operand
-            if(token.equals("+")) {
-                s.push(s.pop() + s.pop());
-            } else if(token.equals("-")) {
-                int first = s.pop();
-                int second = s.pop();
-                s.push(second - first);
-            } else if(token.equals("*")) {
-                s.push(s.pop() * s.pop());
-            } else if(token.equals("/")) {
-                int first = s.pop();
-                int second = s.pop();
-                s.push(second / first);
-            // number
-            } else {
-                s.push(Integer.parseInt(token));
+            switch(token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    stack.push(-(stack.pop() - stack.pop()));
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int first = stack.pop();
+                    int second = stack.pop();
+                    stack.push(second/first);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+                    break;
             }
         }
-        
-        return s.peek();
-        
+        return stack.peek();
     }
 }
